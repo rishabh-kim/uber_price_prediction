6b2ea3e853b4849f88ce50676c442f9847572138
Rishabh Kimothi <rishabhkimothi@Rishabhs-Laptop.local>
2026-01-23 15:30:09 -0600

    initial commit

--- app.py
+++ app.py
@@ -60,6 +60,6 @@ def health():
 
 
 if __name__ == '__main__':
-    # Get port from environment variable or default to 5000
-    port = int(os.environ.get('PORT', 5000))
+    # Get port from environment variable or default to 8000
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port, debug=False)
